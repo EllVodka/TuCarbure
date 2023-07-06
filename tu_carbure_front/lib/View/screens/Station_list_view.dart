ca3bf2c23bd7d8538e65699ce05b9c19d94b963d
EllVodka <eric.sergueev@gmail.com>
2023-07-06 14:22:53 +0200

    feat : add ui for filter

--- tu_carbure_front/lib/View/screens/Station_list_view.dart
+++ tu_carbure_front/lib/View/screens/Station_list_view.dart
@@ -5,6 +5,7 @@ import 'package:tu_carbure_front/Model/Station.dart';
 import 'package:tu_carbure_front/View/screens/Station_detail_view.dart';
 import 'package:tu_carbure_front/View/widget/Station_Favorite_card_widget.dart';
 import 'package:tu_carbure_front/View/widget/Station_card_widget.dart';
+import 'package:tu_carbure_front/View/widget/Station_popup_filter_widget.dart';
 import 'package:tu_carbure_front/ViewModel/Station_favorite_view_model.dart';
 import 'package:tu_carbure_front/ViewModel/Station_view_model.dart';
 
@@ -17,6 +18,7 @@ class StationListView extends StatefulWidget {
 
 class _StationListViewState extends State<StationListView> {
   final StationViewModel _viewmodel = StationViewModel();
+  bool isModalVisible = false;
   List<Station> stations = [];
   List<Station> stationNotFavorite = [];
   Station stationFavorite = emptyStation();
@@ -28,89 +30,120 @@ class _StationListViewState extends State<StationListView> {
   }
 
   void selectStation(BuildContext context, Station station) {
-    Navigator.of(context).pushNamed(StationDetailView.routeName, arguments: {
-      "id": station.id
-    }).then((value) async {
+    Navigator.of(context).pushNamed(StationDetailView.routeName, arguments: {"id": station.id}).then((value) async {
       final prefs = await SharedPreferences.getInstance();
       var favoriteId = (prefs.getString('stationFavoriteId') ?? '');
       setState(() {
         stationFavorite = stations.firstWhere(
-                (element) => element.id == favoriteId,
-            orElse: () => emptyStation());
-        stationNotFavorite =
-            stations.where((station) => station.id != favoriteId ).toList();
+              (element) => element.id == favoriteId,
+          orElse: () => emptyStation(),
+        );
+        stationNotFavorite = stations.where((station) => station.id != favoriteId).toList();
       });
     });
   }
 
   void _fetchStation() async {
+    final prefs = await SharedPreferences.getInstance();
     List<Station> fetchedStation = await _viewmodel.fetchStation();
-    var favoriteId = StationFavoriteViewModel.getFavoriteStationId();
+    var favoriteId = (prefs.getString('stationFavoriteId') ?? '');
     setState(() {
       stations = fetchedStation;
       stationFavorite = stations.firstWhere(
-              (element) => element.id == favoriteId,
-          orElse: () => emptyStation());
-      stationNotFavorite =
-          stations.where((station) => !station.isFavorite!).toList();
+            (element) => element.id == favoriteId,
+        orElse: () => emptyStation(),
+      );
+      stationNotFavorite = stations.where((station) => station.id != favoriteId).toList();
+    });
+  }
+
+  void _toggleModal() {
+    setState(() {
+      isModalVisible = !isModalVisible;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: const Color.fromRGBO(6, 7, 14, 1),
-      child: Column(
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(6, 7, 14, 1),
+      body: Stack(
         children: [
-          Container(
-            margin: const EdgeInsets.only(top: 20.0),
-            child: Image.asset(
-              'assets/tu_carbure_logo.png',
-              width: 150,
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
+          Column(
             children: [
               Container(
-                margin: const EdgeInsets.only(top: 10.0),
-                child: const Text(
-                  ' B7',
-                  style: TextStyle(
-                      fontSize: 16.0, color: Color.fromRGBO(244, 244, 246, 1)),
+                margin: const EdgeInsets.only(top: 20.0),
+                child: Image.asset(
+                  'assets/tu_carbure_logo.png',
+                  width: 150,
                 ),
               ),
-              const SizedBox(width: 50),
-              ElevatedButton(
-                onPressed: () {
-                  // Logique du bouton
-                },
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromRGBO(7, 26, 79, 1)),
-                child: const Text('Filtre'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(top: 10.0),
+                    child: const Text(
+                      ' B7',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Color.fromRGBO(244, 244, 246, 1),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 50),
+                  ElevatedButton(
+                    onPressed:() {
+                      _toggleModal();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromRGBO(7, 26, 79, 1),
+                    ),
+                    child: const Text('Filtre'),
+                  ),
+                ],
+              ),
+              InkWell(
+                onTap: () => selectStation(context, stationFavorite),
+                child: StationFavoriteCard(stationFavorite, 79, 59, 8, 1),
+              ),
+              Expanded(
+                child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 1.4,
+                  ),
+                  itemCount: stationNotFavorite.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final station = stationNotFavorite[index];
+                    return InkWell(
+                      onTap: () => selectStation(context, station),
+                      child: StationCard(station, 7, 26, 79, 1),
+                    );
+                  },
+                ),
               ),
             ],
           ),
-          InkWell(
-            onTap: () => selectStation(context, stationFavorite),
-            child: StationFavoriteCard(stationFavorite, 79, 59, 8, 1),
-          ),
-          Expanded(
-            child: GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 1.4,
-              ),
-              itemCount: stationNotFavorite.length,
-              itemBuilder: (BuildContext context, int index) {
-                final station = stationNotFavorite[index];
-                return InkWell(
-                  onTap: () => selectStation(context, station),
-                  child: StationCard(station, 7, 26, 79, 1),
-                );
+          if (isModalVisible)
+            GestureDetector(
+              onTap: () {
+                _toggleModal();
               },
+              child: Container(
+                color: Colors.black.withOpacity(0.3),
+              ),
             ),
-          ),
+          if (isModalVisible)
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                color: Color.fromRGBO(6, 7, 14, 1),
+                child: StationPopupFilter(),
+              ),
+            ),
         ],
       ),
     );
